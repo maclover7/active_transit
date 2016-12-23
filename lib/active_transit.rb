02debede08a5e88da031bbbcc6ba175ced3d91e7
Jon Moss <me@jonathanmoss.me>
2016-12-23 14:28:25 -0500

    Allow parameters to be passed to #initialize, needed for WmataService

--- lib/active_transit.rb
+++ lib/active_transit.rb
@@ -1,9 +1,9 @@
 require 'active_support/core_ext/string/inflections'
 
 module ActiveTransit
-  def self.service(name)
+  def self.service(name, *args)
     name = name.to_s.downcase
     require "active_transit/services/#{name}_service"
-    ActiveTransit.const_get("#{name}_service".camelize).new
+    ActiveTransit.const_get("#{name}_service".camelize).new(*args)
   end
 end
--- lib/active_transit/services/wmata_service.rb
+++ lib/active_transit/services/wmata_service.rb
@@ -2,6 +2,8 @@ require 'active_transit/service'
 
 module ActiveTransit
   class WmataService < Service
-    #
+    def initialize(api_key)
+      @api_key = api_key
+    end
   end
 end
