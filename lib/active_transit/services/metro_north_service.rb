7cbecfecab3e677deca2c72b96c706b74d4e6b1e
Jon Moss <me@jonathanmoss.me>
2016-12-23 15:56:49 -0500

    Add MetroNorthService#departures_for_station_name

--- lib/active_transit/services/metro_north_service.rb
+++ lib/active_transit/services/metro_north_service.rb
@@ -1,6 +1,7 @@
 module ActiveTransit
   class MetroNorthService < Service
     STATION_ENDPOINT = 'http://as0.mta.info/mnr/mstations/station_status_display.cfm'
+    STATIONS_ENDPOINT = 'http://as0.mta.info/mnr/mstations/default.cfm'
     TRAIN_COLORS = {
       "#EE0034" => "New Haven", "#0039A6" => "Harlem", "#009B3A" => "Hudson"
     }
@@ -29,10 +30,35 @@ module ActiveTransit
       serialized_trains
     end
 
+    def departures_for_station_name(station_name)
+      station = fetch_stations.select { |name, _id| name == station_name }
+      departures_for_station_id(station[station_name])
+    end
+
     private
 
     def fetch_station_data(station_id)
       RestClient.post(STATION_ENDPOINT, { 'P_AVIS_ID': station_id, 'Get Train Status': 'Get Train Status' }).body
+    end
+
+    def fetch_stations
+      @stations ||= begin
+        page = RestClient.get(STATIONS_ENDPOINT)
+        page = Nokogiri::HTML(page)
+
+        raw_stations = page.css('option').select do |el|
+          !el.attr('value').nil?
+        end
+
+        serialized_stations = {}
+
+        raw_stations.each do |station|
+          id, name = station.attr('value').split(',')
+          serialized_stations[name] = id
+        end
+
+        serialized_stations
+      end
     end
   end
 end
