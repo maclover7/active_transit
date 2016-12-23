dde7f94cbabd78994e3da393d7be6e9436f43acd
Jon Moss <me@jonathanmoss.me>
2016-12-23 13:54:42 -0500

    Add ActiveTransit#service API

--- lib/active_transit.rb
+++ lib/active_transit.rb
@@ -1,2 +1,7 @@
+require 'active_support/core_ext/string/inflections'
+
 module ActiveTransit
+  def self.service(name)
+    ActiveTransit.const_get("#{name.to_s.downcase}_service".camelize)
+  end
 end
