44d191904311818b2a865b171003420f30c96113
Jon Moss <me@jonathanmoss.me>
2016-12-23 14:58:13 -0500

    Add WmataService#departures_for_station_id and #departures_for_station_name

--- active_transit.gemspec
+++ active_transit.gemspec
@@ -24,5 +24,6 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency 'rest-client', '~> 2'
 
   spec.add_development_dependency 'bundler', '~> 1.10'
+  spec.add_development_dependency 'pry'
   spec.add_development_dependency 'rake', '~> 10.0'
 end
--- lib/active_transit/services/wmata_service.rb
+++ lib/active_transit/services/wmata_service.rb
@@ -1,9 +1,47 @@
 require 'active_transit/service'
 
+require 'rest-client'
+require 'json'
+
 module ActiveTransit
   class WmataService < Service
+    STATION_ENDPOINT = 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/'
+    STATIONS_ENDPOINT = 'https://api.wmata.com/Rail.svc/json/jStations'
+
     def initialize(api_key)
       @api_key = api_key
+    end
+
+    def departures_for_station_id(station_id)
+      fetch_station_data(station_id)
+    end
+
+    def departures_for_station_name(station_name)
+      stations = fetch_stations.select { |s| s['Name'] == station_name }
+      trains = []
+
+      stations.each do |station|
+        trains << departures_for_station_id(station['Code'])
+      end
+
+      trains
+    end
+
+    private
+
+    def fetch_station_data(station_id)
+      url = STATION_ENDPOINT + station_id
+      trains = RestClient.get(url, params: { 'api_key': @api_key }).body
+      trains = JSON.parse(trains)['Trains']
+      trains
+    end
+
+    def fetch_stations
+      @stations ||= begin
+        stations = RestClient.get(STATIONS_ENDPOINT, params: { 'api_key': @api_key }).body
+        stations = JSON.parse(stations)['Stations']
+        stations
+      end
     end
   end
 end
